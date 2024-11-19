89fb690b8bff0a82cdcecea47969f579c40cd6ee
Rinoffe <irinnabilah@gmail.com>
2024-11-19 19:50:01 +0700

    v3

--- testGit.cpp
+++ testGit.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 main (){
     cout << "hello world\n";
-    cout << "repo lokal tersambung repo remote github"; 
+    cout << "repo lokal tersambung repo remote github\n";
+    cout << "push kode ke github";
 }
